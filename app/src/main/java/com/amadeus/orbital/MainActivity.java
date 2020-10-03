1c6585c637972b4af26b021946bf68b7fb937a43
Siddharth280301 <siddharthpardeshi280301@gmail.com>
2020-10-03 14:33:18 +0530

    Splash activity added.

--- app/src/main/java/com/amadeus/orbital/MainActivity.java
+++ app/src/main/java/com/amadeus/orbital/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
