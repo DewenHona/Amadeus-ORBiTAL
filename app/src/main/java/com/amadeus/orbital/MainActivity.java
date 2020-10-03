323263627982fd2de523f9df3a4faa2b46bd7e75
atharvapatil22 <atharva.22.patil@gmail.com>
2020-10-03 22:59:14 +0530

    1)removed ventura placemarks. 2)added searchweb button

--- app/src/main/java/com/amadeus/orbital/MainActivity.java
+++ app/src/main/java/com/amadeus/orbital/MainActivity.java
@@ -1,12 +1,16 @@
 package com.amadeus.orbital;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
