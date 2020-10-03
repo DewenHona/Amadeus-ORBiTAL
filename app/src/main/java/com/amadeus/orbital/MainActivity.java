6ea6edbd4ed2e1c361239bdb1c98d153f6ff6be3
Siddharth280301 <siddharthpardeshi280301@gmail.com>
2020-10-03 17:04:21 +0530

    colors

--- app/src/main/java/com/amadeus/orbital/MainActivity.java
+++ app/src/main/java/com/amadeus/orbital/MainActivity.java
@@ -1,21 +1,13 @@
 package com.amadeus.orbital;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
-import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.ar.core.exceptions.UnavailableApkTooOldException;
-import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
-import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
-import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 
 public class MainActivity extends AppCompatActivity {
 
