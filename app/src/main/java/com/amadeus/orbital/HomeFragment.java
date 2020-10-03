4fcfb57ff60a1023f0375cf442d3daad048348b1
Swaraj_Dolhare <swarajbdolhare@gmail.com>
2020-10-04 00:47:26 +0530

    Home ui!! and other stuff Done

--- app/src/main/java/com/amadeus/orbital/HomeFragment.java
+++ app/src/main/java/com/amadeus/orbital/HomeFragment.java
@@ -1,20 +1,53 @@
 package com.amadeus.orbital;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.zip.Inflater;
 
 public class HomeFragment extends Fragment {
+    View v;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_home,container,false);
+         v=inflater.inflate(R.layout.frag_home,container,false);
+
+        FloatingActionButton gitHubBtn = v.findViewById(R.id.openGitBtn);
+        gitHubBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri uri = Uri.parse("https://github.com/DewenHona/Amadeus-ORBiTAL");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+        FloatingActionButton nasaOpenBtn = v.findViewById(R.id.openNasaBtn);
+        nasaOpenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri uri = Uri.parse("https://2020.spaceappschallenge.org/challenges/connect/orbital-sky/teams/amadeus/project");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+
+        return v;
     }
 }
